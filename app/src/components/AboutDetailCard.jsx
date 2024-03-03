17843b317334228ea9cf73e39ae9b5c58cb65af3
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-03 17:38:52 +0100

    add icon and  text

--- app/src/components/AboutDetailCard.jsx
+++ app/src/components/AboutDetailCard.jsx
@@ -28,30 +28,24 @@ function AboutDetailCard() {
         </div>
         {/* for what i do section  */}
         <div className="px-4 md:px-14 py-5  ">
-          <h1 className="text-2xl text-black dark:text-white font-semibold">What I do!</h1>
+          <h1 className="text-2xl text-black dark:text-white font-semibold">Ce que je fais!</h1>
           {/* for what do box  */}
           <div className="grid grid-cols-1 md:grid-cols-2 ">
             {/* first box  */}
             <div className="bg-pink-50 dark:bg-slate-900  rounded-lg mr-4 my-4 p-3 sm:flex sm:space-x-2 space-y-2 sm:space-y-0 ">
               {/* for icon  */}
-              <div className="">
-                <svg
-                  className="h-6 w-10 my-2 fill-blue-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 640 512"
-                >
-                  <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" />
-                </svg>
+              <div className="text-lime-600">
+              <svg xmlns="http://www.w3.org/2000/svg" width="2.6em" height="2.6em" viewBox="0 0 24 24"><path fill="currentColor" d="m.586 12l4.95-4.95L6.95 8.464L3.414 12l3.536 3.536l-1.414 1.414zm8.201 8.728l4.486-17.94l1.94.485l-4.485 17.94zm8.263-5.192L20.586 12L17.05 8.464l1.415-1.414l4.95 4.95l-4.95 4.95z"/></svg>
               </div>
               {/* for text  */}
               <div className=" space-y-2">
                 <h1 className="text-lg  font-semibold">
-                  Mern Stack Developer
+                Front-End Development
                 </h1>
-                <p className="text-md">
-                  I have a good knowledge in frontend as well as backend. I have
-                  created lots of projects in Mern Stack.
+                <p className="text-md"> 
+                Je suis un développeur Front-End passionné qui excelle dans la création d'expériences utilisateur 
+                exceptionnelles grâce à une combinaison experte.
+
                 </p>
               </div>
             </div>
@@ -59,24 +53,36 @@ function AboutDetailCard() {
             {/* second box  */}
             <div className="bg-gray-50 dark:bg-slate-900 rounded-lg mr-4 my-4 p-3 sm:flex sm:space-x-2 space-y-2 sm:space-y-0 ">
               {/* for icon  */}
-              <div className="">
-                <svg
-                  className="h-6 w-10 my-2 fill-blue-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 640 512"
-                >
-                  <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" />
-                </svg>
+              <div className="text-lime-600">
+              <svg  xmlns="http://www.w3.org/2000/svg" width="2.6em" height="2.6em" viewBox="0 0 1024 1024"><path fill="currentColor" d="M832 64H192c-17.7 0-32 14.3-32 32v224h704V96c0-17.7-14.3-32-32-32M288 232c-22.1 0-40-17.9-40-40s17.9-40 40-40s40 17.9 40 40s-17.9 40-40 40M160 928c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V704H160zm128-136c22.1 0 40 17.9 40 40s-17.9 40-40 40s-40-17.9-40-40s17.9-40 40-40M160 640h704V384H160zm128-168c22.1 0 40 17.9 40 40s-17.9 40-40 40s-40-17.9-40-40s17.9-40 40-40"/></svg>
               </div>
               {/* for text  */}
               <div className="space-y-2">
                 <h1 className="text-lg  font-semibold">
-                  Competitive programmer
+                Backend Development
                 </h1>
                 <p className="text-md">
-                  I have a good knowledge of programming and I have a lots of
-                  problems of Data Structure and Algorithm in C++.{" "}
+                j'ai déjà commencé à explorer les concepts fondamentaux de la création d'applications robustes
+                 et évolutives côté serveur. Je suis passionné par l'apprentissage continu et 
+                je suis prêt à relever de nouveaux défis pour perfectionner mes compétences en développement backend.
+                </p>
+              </div>
+            </div>
+
+            <div className="bg-gray-50 dark:bg-slate-900 rounded-lg mr-4 my-4 p-3 sm:flex sm:space-x-2 space-y-2 sm:space-y-0 ">
+              {/* for icon  */}
+              <div className="text-lime-600">
+              <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.5 21H8a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8m2 8l2-2l-2-2m-3 0l-2 2l2 2M11 4h2m-1 13v.01"/></svg>
+              </div>
+              {/* for text  */}
+              <div className="space-y-2">
+                <h1 className="text-lg  font-semibold">
+                Mobile Apps
+                </h1>
+                <p className="text-md">
+                j'ai déjà commencé à explorer les concepts fondamentaux de la création d'applications robustes
+                 et évolutives côté serveur. Je suis passionné par l'apprentissage continu et 
+                je suis prêt à relever de nouveaux défis pour perfectionner mes compétences en développement backend.
                 </p>
               </div>
             </div>
@@ -86,10 +92,7 @@ function AboutDetailCard() {
             
           </div>
           {/* third box  */}
-          <div className="px-4 md:px-14 py-5 bg-yellow-50 dark:bg-slate-900 my-3 rounded-xl">I can write quality code and which is easily understandable by Users.</div>
-          <div className="px-4 md:px-14 py-5 bg-purple-50 dark:bg-slate-900 my-3 rounded-xl">I does not only develop software but I can deploy on hosting websites.</div>
-          
-          <div className="px-4 md:px-14 py-5 bg-purple-50 dark:bg-slate-900 my-3 rounded-xl">I have ability to deliver good software with creative ideas and I also have ability to work on Team- Coordination </div>
+
 
           
         </div>
